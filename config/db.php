dadb3d233357a49b30b3826d28bc52a54f28f62e
riasta@yandex.ru <riasta@yandex.ru>
2019-05-29 18:37:06 +0300

    Changed localhost to 127.0.0.1 in db connection to best perfomance

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=lea',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=lea',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
